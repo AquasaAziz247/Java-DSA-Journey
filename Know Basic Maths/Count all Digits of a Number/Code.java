6d952bedc3cfd48af05e8e0f4d356198bf6dbae9
Aquasa Aziz <119380157+AquasaAziz247@users.noreply.github.com>
2026-08-08 23:15:46 +0530

    Add digit counting solutions in Code.java
    Implemented two approaches to count digits in a number.

---
+++ Know Basic Maths/Count all Digits of a Number/Code.java
@@ -0,0 +1,27 @@
+//My Approach
+class Solution {
+    public int countDigit(int n) {
+        int count=0;
+        while(n!=0){
+            int digit = n%10;
+            n=n/10;
+            if(digit>=0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+//Strivers Approach
+class Solution {
+    public int countDigit(int n) {
+        int count=0;
+      if(n==0){
+        return 1;
+      }
+        while(n!=0){
+            n=n/10;
+                count++;
+        }
+        return count;
+    }
